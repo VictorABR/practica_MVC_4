e2f30f9230a69000d44f6e540558ffe7e73787a1
victor.bernardino@uabc.edu.mx <victor.bernardino@uabc.edu.mx>
2021-10-29 00:02:12 -0700

    Agregar archivos de proyecto.

---
+++ practica_MVC_4/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using practica_MVC_4.Models;
+
+namespace practica_MVC_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FormularioVisita()
+        {
+            return View();
+        }
+
+        public ActionResult CargaDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();
+            string comentarios = Request.Form["comentarios"].ToString();
+            LibroVisitas libro = new LibroVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+
+        public ActionResult ListadoVisitas()
+        {
+            LibroVisitas libro = new LibroVisitas();
+            string todo = libro.Leer();
+            ViewData["Libro"] = todo;
+            return View();
+        }
+    }
+}
